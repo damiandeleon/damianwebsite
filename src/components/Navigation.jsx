8806af0db590fc948ce8d54d6bd60e88455ecd23
Damian DeLeon <damianmdeleon@gmail.com>
2026-06-29 12:15:16 -0500

    updateing resume and source files

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import resumePdf from "../assets/Resume.pdf";
+import resumePdf from "../assets/DamianResume2026.pdf";
 
 function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
